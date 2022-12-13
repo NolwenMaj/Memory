e2b1976153de9da0bd4933076032902b3d33161a
HortenseLaclaviere <114992888+HortenseLaclaviere@users.noreply.github.com>
2022-12-13 11:13:55 +0100

    mise en page bouton

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ function turnBlue(){
 }
 
 
-function isItAPair (coordonnee1,coordonnee2) {
+function isItAPair (coordonnee1,coordonnee2) { 
+}
 
+function draw(){
+  var ctx = document.getElementById("C1L1").getContext('2d');
+  for (var i = 0; i < 6; i++) {
+    for (var j = 0; j < 6; j++) {
+      ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ',' +
+                       Math.floor(255 - 42.5 * j) + ',0)';
+      ctx.fillRect(j * 25, i * 25, 25, 25);
+    }
+  }
 }
